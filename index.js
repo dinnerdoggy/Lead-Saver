d0e5072236b1099f8c525b6388fd5e281a097d7f
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2024-06-07 14:03:35 -0500

    Creation
    Chrome extension for saving sales leads in a shareable and efficient way.

---
+++ index.js
@@ -0,0 +1,18 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    renderLeads()
+})
+
+// 1. Wrap the code below in a renderLeads() function
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
+}
